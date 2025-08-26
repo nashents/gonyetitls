f38f029c18f35f9dc33bc39624926f1d7eeca5a8
Panashe Ngorima <panashengorima@gmail.com>
2025-08-26 07:56:44 +0200

    changes

--- database/migrations/2025_08_26_075546_add_haulage_type_to_trips_table.php
+++ database/migrations/2025_08_26_075546_add_haulage_type_to_trips_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddHaulageTypeToMigrationsTable extends Migration
+class AddHaulageTypeToTripsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddHaulageTypeToMigrationsTable extends Migration
      */
     public function up()
     {
-        Schema::table('migrations', function (Blueprint $table) {
-              $table->text('haulage_type')->nullable();
+        Schema::table('trips', function (Blueprint $table) {
+             $table->text('haulage_type')->nullable();
         });
     }
 
@@ -25,8 +25,8 @@ class AddHaulageTypeToMigrationsTable extends Migration
      */
     public function down()
     {
-        Schema::table('migrations', function (Blueprint $table) {
-           $table->dropColumn('haulage_type');
+        Schema::table('trips', function (Blueprint $table) {
+             $table->dropColumn('haulage_type');
         });
     }
 }
