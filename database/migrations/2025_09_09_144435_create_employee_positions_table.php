f4d84749e7e288277297d3c4f30d975c0e75662b
Panashe Ngorima <panashengorima@gmail.com>
2025-09-09 15:15:42 +0200

    changes

--- database/migrations/2025_09_09_144435_create_employee_positions_table.php
+++ database/migrations/2025_09_09_144435_create_employee_positions_table.php
@@ -13,34 +13,55 @@ class CreateEmployeePositionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('employee_positions', function (Blueprint $table) {
+         Schema::create('employee_positions', function (Blueprint $table) {
             $table->id();
                // Employee link
             $table->foreignId('employee_id')
+                 ->nullable()
+                   ->unsigned()
                   ->constrained('employees')
                   ->onDelete('cascade');
 
             // Link to standardized job title
             $table->foreignId('job_title_id')
+                  ->nullable()
+                    ->unsigned()
                   ->constrained('job_titles')
                   ->onDelete('restrict');
 
             $table->foreignId('rank_id')
+                  ->nullable()
+                    ->unsigned()
                   ->constrained('ranks')
                   ->onDelete('restrict');
 
             // Link to standardized grade
             $table->foreignId('grade_id')
+                  ->nullable()
+                    ->unsigned()
                   ->constrained('grades')
                   ->onDelete('restrict');
 
-            $table->date('start_date');
+            $table->foreignId('branch_id')
+                  ->nullable()
+                    ->unsigned()
+                  ->constrained('branches')
+                  ->onDelete('restrict');
+
+            $table->foreignId('department_id')
+                  ->nullable()
+                  ->unsigned()
+                  ->constrained('departments')
+                  ->onDelete('restrict');
+
+            $table->date('start_date')->nullable();
             $table->date('end_date')->nullable();
 
-            $table->string('change_reason');
+            $table->string('change_reason')->nullable();
 
             $table->foreignId('changed_by')
                   ->nullable()
+                    ->unsigned()
                   ->constrained('users')
                   ->nullOnDelete();
 
