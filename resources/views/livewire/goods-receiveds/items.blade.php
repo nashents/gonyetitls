c41f6ab5f24f961c271344db186cbef2a57e9730
nashents <panashengorima@gmail.com>
2026-07-21 16:23:44 +0200

    changes

--- resources/views/livewire/goods-receiveds/items.blade.php
+++ resources/views/livewire/goods-receiveds/items.blade.php
@@ -3,9 +3,16 @@
       
         <table class="table  table-striped table-bordered table-sm table-responsive" cellspacing="0" width="100%">
             <thead >
+              <tr>
                 <th class="th-sm">Product
                 </th>
                 <th class="th-sm">SN#
+                </th>
+                <th class="th-sm text-right">Qty
+                </th>
+                <th class="th-sm text-right">Unit Cost
+                </th>
+                <th class="th-sm text-right">Subtotal
                 </th>
               </tr>
             </thead>
@@ -14,19 +21,30 @@
               @forelse ($items as $item)
                 <tr>
                   <td>{{$item->product->brand ? $item->product->brand->name : ""}} {{$item->product ? $item->product->name : ""}}</td>
-                  <td>{{$item->serial_number ? "SN#: ".$item->serial_number : ""}} {{$item->product->identification_number ? "ID#: ".$item->product->identification_number : ""}}</td> 
+                  <td>{{$item->serial_number ? "SN#: ".$item->serial_number : ""}} {{$item->product->identification_number ? "ID#: ".$item->product->identification_number : ""}}</td>
+                  <td class="text-right">{{number_format($item->qty ?? 1, 2)}}</td>
+                  <td class="text-right">{{number_format($item->amount ?? 0, 2)}}</td>
+                  <td class="text-right">{{number_format($item->subtotal ?? 0, 2)}}</td>
                 </tr>
               @empty
                 <tr>
-                  <td colspan="2">
+                  <td colspan="5">
                       <div style="text-align:center; text-color:grey; padding-top:5px; padding-bottom:5px; font-size:17px">
                           No Items Found ....
                       </div>
-                      
+
                   </td>
-                </tr>  
+                </tr>
               @endforelse
             </tbody>
+            @if (isset($items) && $items->isNotEmpty())
+            <tfoot>
+              <tr>
+                <th colspan="4" class="text-right">Total</th>
+                <th class="text-right">{{number_format($items->sum('subtotal'), 2)}}</th>
+              </tr>
+            </tfoot>
+            @endif
             @else
                 <img style="padding-left: 35%; padding-top:7%; width:100% height:100%" src="{{asset('images/nodata.png')}}" alt="">
              @endif
