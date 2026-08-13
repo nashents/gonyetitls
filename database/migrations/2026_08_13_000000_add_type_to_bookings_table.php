cb0daa6a3aec770decdc3dcdb61974620b624a6d
nashents <panashengorima@gmail.com>
2026-08-13 13:20:30 +0200

    changes

---
+++ database/migrations/2026_08_13_000000_add_type_to_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTypeToBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasColumn('bookings', 'type')) {
+            Schema::table('bookings', function (Blueprint $table) {
+                $table->text('type')->nullable();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('bookings', 'type')) {
+            Schema::table('bookings', function (Blueprint $table) {
+                $table->dropColumn('type');
+            });
+        }
+    }
+}
